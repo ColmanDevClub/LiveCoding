358da18a039a8c720f1e020ec53a4641184a8f60
danielmargolin <margolin23@gmail.com>
2022-12-12 12:04:00 +0200

    fixes

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
+const totalSizeInBytes = 100000000;
+
+let usedSize = 0;
 
 const initApp = () => {
   usedSize = Number(window.localStorage.getItem("usedSize"));
   updateDashboard(usedSize);
 }
 
-const onFileInputChange = (event) => {
-  const fileName = event.value;
+const onFileInputChange = (fileInputElement) => {
+  const fileName = fileInputElement.value;
   const isImgFile = /\.(gif|jpe?g|tiff?|png|webp|bmp)$/i.test(fileName);
 
   if (isImgFile) {
-    const file = event.files[0];
+    const file = fileInputElement.files[0];
     updateFile(file);
   } else {
     alert("Invalid file type!");
@@ -18,18 +21,9 @@ const onFileInputChange = (event) => {
 
 const updateFile = (file) => {
   const fileSize = file.size;
-  if (usedSize + fileSize < totalSize) {
-    //------------------- with files storing
-    // files.push(file);
-    // let totalFilesSize = 0;
-    // for (let i = 0; i < files.length; i++) {
-    //   totalFilesSize += files[i].size;
-    // }
-    // usedSize = totalFilesSize;
-    //-------------------
-    //------------------- without files storing
+
+  if (usedSize + fileSize < totalSizeInBytes) {
     usedSize = usedSize + fileSize;
-    //-------------------
     updateDashboard(usedSize);
     window.localStorage.setItem("usedSize", usedSize);
   } else {
@@ -37,44 +31,44 @@ const updateFile = (file) => {
   } 
 }
 
-
 const updateDashboard = (usedSize) => {
-  const formattedUsedSize = getFormatBytes(usedSize)
+  const formattedUsedSize = getFormattedBytes(usedSize)
   const usedSizeElement = document.getElementById("used-size");
   usedSizeElement.innerText = formattedUsedSize.size;
   const usedSizeUnitElement = document.getElementById("used-size-unit");
   usedSizeUnitElement.innerText = formattedUsedSize.unit;
 
-  const formattedRemainedSize = getFormatBytes(totalSize - usedSize)
+  const formattedRemainSize = getFormattedBytes(totalSizeInBytes - usedSize)
   const remainSizeElement = document.getElementById("remain-size");
-  remainSizeElement.innerHTML = formattedRemainedSize.size;
-  const remainedSizeUnitElement = document.getElementById("remain-size-unit");
-  remainedSizeUnitElement.innerText = formattedRemainedSize.unit;
+  remainSizeElement.innerHTML = formattedRemainSize.size;
+  const remainSizeUnitElement = document.getElementById("remain-size-unit");
+  remainSizeUnitElement.innerText = formattedRemainSize.unit;
 
-  const formattedTotalSize = getFormatBytes(totalSize);
+  const formattedTotalSize = getFormattedBytes(totalSizeInBytes);
   const maxSizeElement = document.getElementById("max-size");
   maxSizeElement.innerText = formattedTotalSize.size;
   const totalSizeUnitElement = document.getElementById("total-size-unit");
   totalSizeUnitElement.innerText = formattedTotalSize.unit;
 
   const progressBarClr = document.getElementById("progress-bar-clr");
-  progressBarClr.style.width = (usedSize / totalSize) * 100 + "%";
+  progressBarClr.style.width = (usedSize / totalSizeInBytes) * 100 + "%";
 
   const progressBarCircle = document.getElementById("progress-bar-circle");
   if (usedSize > 0) progressBarCircle.style.right = "0";
   else progressBarCircle.style.right = "-10px";
 }
 
-const getFormatBytes = (bytes, decimals = 2) => {
+const getFormattedBytes = (bytes) => {
+  const decimals = 2;
   if (!+bytes) {
     return {
       size: 0, unit: 'Bytes'
     }
   } 
-  const k = 1024
-  const dm = decimals < 0 ? 0 : decimals
-  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
-  const i = Math.floor(Math.log(bytes) / Math.log(k))
+  const k = 1024;
+  const dm = decimals < 0 ? 0 : decimals;
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
 
   return  {
     size: parseFloat((bytes / Math.pow(k, i)).toFixed(dm)),
